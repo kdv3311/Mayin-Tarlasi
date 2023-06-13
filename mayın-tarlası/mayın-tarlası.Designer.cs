fc49aa76ff2ee8048f3c5a8b62c99de59f58a726
erenb <erenb@EREN>
2023-06-14 00:30:23 +0300

    Add project files.

---
+++ mayın-tarlası/mayın-tarlası.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace mayın_tarlası
+{
+    partial class mayın_tarlası
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            groupBox1 = new GroupBox();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            radioButton3 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
+            puan = new Label();
+            puantext = new Label();
+            mayın = new Label();
+            label2 = new Label();
+            groupBox2 = new GroupBox();
+            dataGrid = new DataGridView();
+            BottomToolStripPanel = new ToolStripPanel();
+            TopToolStripPanel = new ToolStripPanel();
+            RightToolStripPanel = new ToolStripPanel();
+            LeftToolStripPanel = new ToolStripPanel();
+            ContentPanel = new ToolStripContentPanel();
+            statusStrip1 = new StatusStrip();
+            toolStripStatusLabel1 = new ToolStripStatusLabel();
+            toolStripProgressBar1 = new ToolStripProgressBar();
+            toolStripStatusLabel2 = new ToolStripStatusLabel();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGrid).BeginInit();
+            statusStrip1.SuspendLayout();
+            
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Location = new Point(48, 73);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Size = new Size(470, 470);
+            tableLayoutPanel1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(radioButton3);
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            groupBox1.Location = new Point(661, 4);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(390, 150);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Ayarlar";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(192, 76);
+            button1.Name = "button1";
+            button1.Size = new Size(158, 39);
+            button1.TabIndex = 5;
+            button1.Text = "Oyuna Başla";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(192, 33);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Oyuncu Adı";
+            textBox1.Size = new Size(158, 29);
+            textBox1.TabIndex = 3;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(27, 83);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(52, 25);
+            radioButton3.TabIndex = 2;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "Zor";
+            radioButton3.UseVisualStyleBackColor = true;
+            radioButton3.CheckedChanged += radioButton3_CheckedChanged;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(27, 58);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(81, 25);
+            radioButton2.TabIndex = 1;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Normal";
+            radioButton2.UseVisualStyleBackColor = true;
+            radioButton2.CheckedChanged += radioButton2_CheckedChanged;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(27, 33);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(66, 25);
+            radioButton1.TabIndex = 0;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Kolay";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // puan
+            // 
+            puan.AutoSize = true;
+            puan.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            puan.Location = new Point(942, 168);
+            puan.Name = "puan";
+            puan.Size = new Size(69, 28);
+            puan.TabIndex = 5;
+            puan.Text = "Puan : ";
+            // 
+            // puantext
+            // 
+            puantext.AutoSize = true;
+            puantext.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            puantext.Location = new Point(1011, 171);
+            puantext.Name = "puantext";
+            puantext.Size = new Size(22, 25);
+            puantext.TabIndex = 6;
+            puantext.Text = "0";
+            // 
+            // mayın
+            // 
+            mayın.AutoSize = true;
+            mayın.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            mayın.Location = new Point(686, 168);
+            mayın.Name = "mayın";
+            mayın.Size = new Size(129, 28);
+            mayın.TabIndex = 7;
+            mayın.Text = "Mayın Sayısı :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(821, 168);
+            label2.Name = "label2";
+            label2.Size = new Size(23, 28);
+            label2.TabIndex = 8;
+            label2.Text = "0";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(dataGrid);
+            groupBox2.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            groupBox2.Location = new Point(661, 217);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(390, 332);
+            groupBox2.TabIndex = 9;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Puan Tablosu";
+            // 
+            // dataGrid
+            // 
+            dataGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGrid.BackgroundColor = SystemColors.MenuHighlight;
+            dataGrid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGrid.Enabled = false;
+            dataGrid.GridColor = SystemColors.ActiveCaption;
+            dataGrid.Location = new Point(6, 33);
+            dataGrid.Name = "dataGrid";
+            dataGrid.RowTemplate.Height = 25;
+            dataGrid.Size = new Size(378, 293);
+            dataGrid.TabIndex = 0;
+            // 
+            // BottomToolStripPanel
+            // 
+            BottomToolStripPanel.Location = new Point(0, 0);
+            BottomToolStripPanel.Name = "BottomToolStripPanel";
+            BottomToolStripPanel.Orientation = Orientation.Horizontal;
+            BottomToolStripPanel.RowMargin = new Padding(3, 0, 0, 0);
+            BottomToolStripPanel.Size = new Size(0, 0);
+            // 
+            // TopToolStripPanel
+            // 
+            TopToolStripPanel.Location = new Point(0, 0);
+            TopToolStripPanel.Name = "TopToolStripPanel";
+            TopToolStripPanel.Orientation = Orientation.Horizontal;
+            TopToolStripPanel.RowMargin = new Padding(3, 0, 0, 0);
+            TopToolStripPanel.Size = new Size(0, 0);
+            // 
+            // RightToolStripPanel
+            // 
+            RightToolStripPanel.Location = new Point(0, 0);
+            RightToolStripPanel.Name = "RightToolStripPanel";
+            RightToolStripPanel.Orientation = Orientation.Horizontal;
+            RightToolStripPanel.RowMargin = new Padding(3, 0, 0, 0);
+            RightToolStripPanel.Size = new Size(0, 0);
+            // 
+            // LeftToolStripPanel
+            // 
+            LeftToolStripPanel.Location = new Point(0, 0);
+            LeftToolStripPanel.Name = "LeftToolStripPanel";
+            LeftToolStripPanel.Orientation = Orientation.Horizontal;
+            LeftToolStripPanel.RowMargin = new Padding(3, 0, 0, 0);
+            LeftToolStripPanel.Size = new Size(0, 0);
+            // 
+            // ContentPanel
+            // 
+            ContentPanel.Size = new Size(150, 175);
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1, toolStripProgressBar1, toolStripStatusLabel2 });
+            statusStrip1.Location = new Point(0, 565);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(1113, 22);
+            statusStrip1.TabIndex = 12;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            toolStripStatusLabel1.Size = new Size(118, 17);
+            toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // toolStripProgressBar1
+            // 
+            toolStripProgressBar1.Margin = new Padding(400, 3, 1, 3);
+            toolStripProgressBar1.Name = "toolStripProgressBar1";
+            toolStripProgressBar1.Size = new Size(100, 16);
+            // 
+            // toolStripStatusLabel2
+            // 
+            toolStripStatusLabel2.Margin = new Padding(340, 3, 0, 2);
+            toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            toolStripStatusLabel2.Size = new Size(118, 17);
+            toolStripStatusLabel2.Text = "toolStripStatusLabel2";
+            // 
+            // mayın_tarlası
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1113, 587);
+            Controls.Add(statusStrip1);
+            Controls.Add(groupBox2);
+            Controls.Add(label2);
+            Controls.Add(mayın);
+            Controls.Add(puantext);
+            Controls.Add(puan);
+            Controls.Add(groupBox1);
+            Controls.Add(tableLayoutPanel1);
+            Name = "mayın_tarlası";
+            Text = "mayın_tarlası";
+            Load += mayın_tarlası_Load;
+            MouseClick += mayın_tarlası_MouseClick;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGrid).EndInit();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private TableLayoutPanel tableLayoutPanel1;
+        private GroupBox groupBox1;
+        private RadioButton radioButton3;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private TextBox textBox1;
+        private Label puan;
+        private Label puantext;
+        private Label mayın;
+        private Label label2;
+        private Button button1;
+        private GroupBox groupBox2;
+        private DataGridView dataGrid;
+        private ToolStripPanel BottomToolStripPanel;
+        private ToolStripPanel TopToolStripPanel;
+        private ToolStripPanel RightToolStripPanel;
+        private ToolStripPanel LeftToolStripPanel;
+        private ToolStripContentPanel ContentPanel;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
+        private ToolStripProgressBar toolStripProgressBar1;
+        private ToolStripStatusLabel toolStripStatusLabel2;
+    }
+}
---
+++ mayın-tarlası/mayın-tarlası.cs
@@ -0,0 +1,405 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+
+namespace mayın_tarlası
+{
+
+    public partial class mayın_tarlası : Form
+    {
+        OleDbConnection con;
+        OleDbDataAdapter da;
+        OleDbCommand cmd;
+        DataSet ds;
+        string bilgilerim = "EREN TAN 17 UYDUSUNDAN SELAMLAR 7 KITA 124 ÜLKEYE YAYIN YAPMAKTAYIZ";
+
+        public mayın_tarlası()
+        {
+            InitializeComponent();
+        }
+
+        int Puan = 0;
+        Random rnd = new Random();
+        List<int> Mayın = new List<int>();
+        int Katsayi = 1;
+
+        private void mayın_tarlası_Load(object sender, EventArgs e)
+        {
+            
+            gridwievDolur();
+            dataGrid.Rows[0].Cells[0].Selected = false;
+
+            bilgileriYaz();
+        }
+        public void gridwievDolur()
+        {
+            dataGrid.Rows.Clear();
+            con = new OleDbConnection("Provider=Microsoft.ACE.OleDb.12.0;Data Source=mayintarlasi.accdb");
+            da = new OleDbDataAdapter("SElect userName,dTarih,puan from Oyuncu", con);
+            ds = new DataSet();
+            con.Open();
+            da.Fill(ds, "Oyuncu");
+            dataGrid.DataSource = ds.Tables["Oyuncu"];
+            con.Close();
+            dataGrid.Rows[0].Cells[0].Selected = true;
+        }
+        private bool isimKontrol(string isim)
+        {
+            con.Open();
+            OleDbCommand komut = new OleDbCommand("SELECT COUNT(*) FROM Oyuncu where userName=@adi", con);
+            komut.Parameters.Add("@adi", isim);
+
+            int ks = int.Parse(komut.ExecuteScalar().ToString());
+            if (ks == 1)
+            {
+                con.Close();
+                return true;
+            }
+            else
+            {
+                con.Close();
+                return false;
+                
+
+            }
+
+
+
+        }
+
+        /*mayın tarlası oluşturma */
+        int Msayisi = 0;
+        private void btndoldur(int mayinSayisi)
+        {
+            int sayı = 0;
+            for (int m = 0; m < mayinSayisi; m++)
+            {
+                sayı = rnd.Next(1, 100);
+                for (int g = 0; g < Mayın.Count; g++)
+                {
+                    if (sayı == Mayın[g])
+                    {
+                        sayı = rnd.Next(1, 100);
+                        g = 0;
+                    }
+                }
+
+                Mayın.Add(sayı);
+            }
+
+            tableLayoutPanel1.ColumnStyles.Clear();
+            tableLayoutPanel1.RowStyles.Clear();
+            tableLayoutPanel1.Controls.Clear();
+
+            int kutu = 1;
+
+
+
+            tableLayoutPanel1.ColumnCount = 10;
+            tableLayoutPanel1.RowCount = 10;
+
+
+
+            for (int i = 0; i < tableLayoutPanel1.ColumnCount; i++)
+            {
+                tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+
+                for (int x = 0; x < tableLayoutPanel1.RowCount; x++)
+                {
+                    if (i == 0)
+                    {
+                        tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+                    }
+                    Button cmd = new Button();
+                    cmd.BackColor = Color.Blue;
+                    cmd.Dock = DockStyle.Fill;
+                    cmd.Name = kutu.ToString();
+                    // cmd.Text = kutu.ToString();
+                    tableLayoutPanel1.Controls.Add(cmd, i, x);
+                    kutu++;
+                }
+            }
+
+            ButtonClickAyarla();
+        }
+        private void kayıt()
+        {
+            cmd = new OleDbCommand();
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "insert into Oyuncu (userName,dTarih,puan) values ('" + textBox1.Text + "','" + String.Format("{0:d}", DateTime.Now) + "','" + Convert.ToInt32(puantext.Text) + "')";
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+        /*------------------------------------------------*/
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                label2.Text = "10";
+                Katsayi = 1;
+            }
+        }
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked == true)
+            {
+                label2.Text = "25";
+                Katsayi = 3;
+            }
+        }
+
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton3.Checked == true)
+            {
+                label2.Text = "40";
+                Katsayi = 5;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            toolStripProgressBar1.Maximum = 100 - Convert.ToInt32(label2.Text);
+            toolStripProgressBar1.Minimum = 0;
+            toolStripProgressBar1.Step = 100 - (Convert.ToInt32(label2.Text));
+            Mayın.Clear();
+
+            if (string.IsNullOrEmpty(textBox1.Text))
+                MessageBox.Show("Oyuncu adı kısmı boş geçilemez");
+            else if (radioButton1.Checked == false && radioButton2.Checked == false && radioButton3.Checked == false)
+                MessageBox.Show("Lütfen oyun seviyesini seçiniz");
+            else if (isimKontrol(textBox1.Text))
+            {
+                MessageBox.Show("kullanıcı zaten var.");
+
+            }
+            else
+            {
+                btndoldur(Convert.ToInt32(label2.Text));
+                radioButton1.Enabled = false;
+                radioButton2.Enabled = false;
+                radioButton3.Enabled = false;
+
+            }
+            Dtiklanan = 1;
+
+            //hile();
+
+        }
+        private void ButtonClickAyarla()
+        {
+            foreach (Control ctl in this.tableLayoutPanel1.Controls)
+            {
+                ctl.MouseClick += new MouseEventHandler(mayın_tarlası_MouseClick);
+
+            }
+
+        }
+
+        private void mayın_tarlası_MouseClick(object sender, MouseEventArgs e)
+        {
+            Event(sender);
+
+        }
+        int Dtiklanan = 1;
+        private void Event(object sender)
+        {
+
+            if (sender.GetType().Name == "Button")
+            {
+                Msayisi = 0;
+                Button tiklanan = (Button)sender;
+                if (tiklanan.BackColor != Color.Green && tiklanan.BackColor != Color.Red)
+                {
+                    string isim = tiklanan.Name;
+                    if (Mayın.IndexOf(Convert.ToInt32(isim)) != -1) // Yandın
+                    {
+                        tiklanan.BackColor = Color.Red;
+                        HepsiniAc();
+                        MessageBox.Show("Kaybettiniz. \nToplam Puan: " + Puan);
+                        Puan = 0;
+                    }
+                    else // Kazandın
+                    {
+
+
+                        tiklanan.BackColor = Color.Green;
+
+
+                        Puan += Katsayi;
+                        //tiklanan.Text = Puan.ToString();
+                        puantext.Text = Puan.ToString();
+
+
+                        if (Dtiklanan == 100 - Convert.ToInt32(label2.Text))
+                        {
+                            HepsiniAc();
+                            MessageBox.Show("Kazandınız \nToplam Puan: " + Puan);
+                            Puan = 0;
+                        }
+                        else
+                        {
+                            Dtiklanan++;
+                            toolStripProgressBar1.Value += 1;
+                        }
+                        foreach (int item in mayınTara(Convert.ToInt32(tiklanan.Name)))
+                        {
+                            for (int k = 0; k < Mayın.Count; k++)
+                            {
+                                if (item == Mayın[k])
+                                {
+                                    Msayisi++;
+                                }
+                            }
+                        }
+                        tiklanan.Text = Msayisi.ToString();
+
+                    }
+                }
+            }
+
+        }
+        /*herhangi oyun sonunu gösteren kod bloğu */
+        private void HepsiniAc()
+        {
+            for (int i = 0; i <= (tableLayoutPanel1.ColumnCount * tableLayoutPanel1.RowCount) - 1; i++)
+            {
+                Button btn = (Button)tableLayoutPanel1.Controls[i];
+                if (Mayın.IndexOf(Convert.ToInt32(btn.Name)) != -1)
+                {
+                    btn.BackColor = Color.Red;
+                    btn.BackgroundImageLayout = ImageLayout.Stretch;
+                    btn.BackgroundImage = Image.FromFile(System.Windows.Forms.Application.StartupPath + "\\mayin.png");
+                }
+                else
+                {
+                    btn.BackColor = Color.Green;
+                }
+
+
+            }
+            kayıt();
+
+
+            this.Controls.Clear();
+            this.InitializeComponent();
+            gridwievDolur();
+            bilgileriYaz();
+            
+
+        }
+        private void bilgileriYaz()
+        {
+            toolStripStatusLabel1.Text = bilgilerim;
+            toolStripStatusLabel2.Text = DateTime.Now.ToString();
+        }
+
+        //hileee- -------------------------------------
+
+        //private void hile()
+        //{
+        //    for (int i = 0; i <= (tableLayoutPanel1.ColumnCount * tableLayoutPanel1.RowCount) - 1; i++)
+        //    {
+        //        Button btn = (Button)tableLayoutPanel1.Controls[i];
+        //        if (Mayın.IndexOf(Convert.ToInt32(btn.Name)) != -1)
+        //        {
+        //            btn.BackColor = Color.Red;
+        //            btn.BackgroundImageLayout = ImageLayout.Stretch;
+        //            btn.BackgroundImage = Image.FromFile(System.Windows.Forms.Application.StartupPath + "\\mayin.png");
+        //        }
+        //    }
+        //}
+
+        List<int> taranacak = new List<int>();
+        private List<int> mayınTara(int tıklananid)
+        {
+            taranacak.Clear();
+            if (tıklananid == 1)
+            {
+                taranacak.Add(tıklananid + 11);
+                taranacak.Add(tıklananid + 10);
+                taranacak.Add(tıklananid + 1);
+            }
+            else if (tıklananid == 10)
+            {
+                taranacak.Add(tıklananid + 9);
+                taranacak.Add(tıklananid + 10);
+                taranacak.Add(tıklananid - 1);
+            }
+            else if (tıklananid == 100)
+            {
+                taranacak.Add(tıklananid - 1);
+                taranacak.Add(tıklananid - 10);
+                taranacak.Add(tıklananid - 11);
+            }
+            else if (tıklananid == 91)
+            {
+                taranacak.Add(tıklananid + 1);
+                taranacak.Add(tıklananid - 9);
+                taranacak.Add(tıklananid - 10);
+            }
+            else if (tıklananid != 1 && tıklananid != 91 && tıklananid % 10 == 1)
+            {
+                taranacak.Add(tıklananid + 10);
+                taranacak.Add(tıklananid - 10);
+                taranacak.Add(tıklananid - 9);
+                taranacak.Add(tıklananid + 11);
+                taranacak.Add(tıklananid + 1);
+            }
+            else if (tıklananid != 1 && tıklananid < 10)
+            {
+                taranacak.Add(tıklananid + 10);
+                taranacak.Add(tıklananid - 1);
+                taranacak.Add(tıklananid + 1);
+                taranacak.Add(tıklananid + 9);
+                taranacak.Add(tıklananid + 11);
+            }
+            else if (tıklananid != 10 && tıklananid != 100 && tıklananid % 10 == 0)
+            {
+                taranacak.Add(tıklananid + 10);
+                taranacak.Add(tıklananid - 10);
+                taranacak.Add(tıklananid + 9);
+                taranacak.Add(tıklananid - 11);
+                taranacak.Add(tıklananid - 1);
+            }
+            else if (tıklananid != 91 && tıklananid != 100 && tıklananid / 10 == 9)
+            {
+                taranacak.Add(tıklananid - 10);
+                taranacak.Add(tıklananid + 1);
+                taranacak.Add(tıklananid - 1);
+                taranacak.Add(tıklananid - 11);
+                taranacak.Add(tıklananid - 9);
+            }
+            else
+            {
+                taranacak.Add(tıklananid + 10);
+                taranacak.Add(tıklananid + 1);
+                taranacak.Add(tıklananid - 1);
+                taranacak.Add(tıklananid - 10);
+                taranacak.Add(tıklananid - 11);
+                taranacak.Add(tıklananid + 11);
+                taranacak.Add(tıklananid - 9);
+                taranacak.Add(tıklananid + 9);
+            }
+            return taranacak;
+
+
+
+        }
+
+      
+    }
+}
